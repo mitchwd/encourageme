f0be0f334225393a3cd6e0e1dcde9cc50164534f
@mitchwd <mitch@mitchwd.com>
2014-08-16 22:03:09 +0930

    Add letter validations

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -2,4 +2,6 @@ class Letter < ActiveRecord::Base
   belongs_to :user
 
   enum status: [:pending, :published, :flagged, :hidden] # DB Integers [0: Pending, 1: Published, 2: Flagged, 3: Hidden]
+
+  validates_presence_of :content, :status
 end
