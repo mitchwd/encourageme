49c28480d2b88d89b4155850f7d9081716138e80
@mitchwd <mitch@mitchwd.com>
2014-08-16 22:21:13 +0930

    Set default publishing state

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,4 +1,11 @@
 class Letter < ActiveRecord::Base
+	after_initialize do 
+    # Set default publishing state
+    if self.new_record?
+      self.status = 1
+    end
+  end
+
   belongs_to :user
 
   enum status: [:pending, :published, :flagged, :hidden] # DB Integers [0: Pending, 1: Published, 2: Flagged, 3: Hidden]
